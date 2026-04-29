cb6748cc9f40827addc4f4937d1a6fcf1cfea8a1
pozeratel <166392230+pozeratel@users.noreply.github.com>
2026-04-29 23:31:43 +0300

    added isOnline check

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,7 +3,7 @@ import style from "../FriendListItem/FriendListItem.module.css";
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={style.friendItem}>
-      <span className={isOnline ? "status online" : "status offline"}></span>
+      <span className={isOnline ? style.online : style.offline}></span>
       <img className={style.avatar} src={avatar} alt={name} />
       <p className={style.name}>{name}</p>
     </li>
